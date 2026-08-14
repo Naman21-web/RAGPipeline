8a60765c2c71dfbcc2e7ebdefd27456f846f9036
Naman Jain <jainn0984@gmail.com>
2026-08-14 17:45:55 +0530

    Created LangGraph States

---
+++ src/rag/state.ts
@@ -0,0 +1,18 @@
+import { Annotation } from "@langchain/langgraph";
+import type { Document } from "@langchain/core/documents";
+
+export const RAGState = Annotation.Root({
+  question: Annotation<string>,
+
+  documents: Annotation<Document[]>({
+    default: () => [],
+  }),
+
+  isRelevant: Annotation<boolean>({
+    default: () => false,
+  }),
+
+  relevanceReason: Annotation<string>({
+    default: () => "",
+  }),
+});
