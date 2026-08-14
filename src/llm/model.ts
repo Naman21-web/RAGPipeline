2137661ea09c45725ea29b85dd1aa5027fa62b73
Naman Jain <jainn0984@gmail.com>
2026-08-14 17:45:07 +0530

    Added chat model for response parsing

--- src/llm/model.ts
+++ src/llm/model.ts
@@ -1,6 +1,12 @@
-import { GoogleGenerativeAIEmbeddings } from "@langchain/google-genai";
+import { GoogleGenerativeAIEmbeddings,ChatGoogleGenerativeAI } from "@langchain/google-genai";
 
 export const embeddings = new GoogleGenerativeAIEmbeddings({
   model: "gemini-embedding-001",
   apiKey: process.env.GOOGLE_API_KEY,
+});
+
+export const llm = new ChatGoogleGenerativeAI({
+  model: "gemini-3.6-flash",
+  apiKey: process.env.GOOGLE_API_KEY,
+  temperature: 0,
 });
